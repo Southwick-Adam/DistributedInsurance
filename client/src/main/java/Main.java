6f03dd0e63690c7e2fc79e8b73f4bff24921cdba
Adam Southwick <adamsouthwick100@gmail.com>
2024-10-07 17:48:20 +0100

    fixed output

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -6,6 +6,8 @@ import service.core.ClientInfo;
 import service.core.Constants;
 import service.core.Quotation;
 
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[]args) {
 
@@ -15,16 +17,48 @@ public class Main {
             
             BrokerService service = (BrokerService) registry.lookup(Constants.BROKER_SERVICE);
 
-            ClientInfo ci = new ClientInfo("Niki Collier", ClientInfo.FEMALE, 49, 1.5494, 80, false, false);
-        
-            List<Quotation> quotesList = service.getQuotations(ci);
-            for (Quotation q : quotesList) {
-                System.out.println(q.company + ", " + q.reference + ", " + q.price);
+            for (ClientInfo ci : clients) {
+                displayProfile(ci);
+                List<Quotation> quotesList = service.getQuotations(ci);
+                for (Quotation q : quotesList) {
+                    displayQuotation(q);
+                }
             }
-            System.out.println("LIST END");
 
         } catch(Exception e) {
             System.out.println("Client Trouble: " + e);
         }
     }
+
+    public static void displayProfile(ClientInfo info) {
+		System.out.println("|=================================================================================================================|");
+		System.out.println("|                                     |                                     |                                     |");
+		System.out.println(
+				"| Name: " + String.format("%1$-29s", info.name) + 
+				" | Gender: " + String.format("%1$-27s", (info.gender==ClientInfo.MALE?"Male":"Female")) +
+				" | Age: " + String.format("%1$-30s", info.age)+" |");
+		System.out.println(
+				"| Weight/Height: " + String.format("%1$-20s", info.weight+"kg/"+info.height+"m") + 
+				" | Smoker: " + String.format("%1$-27s", info.smoker?"YES":"NO") +
+				" | Medical Problems: " + String.format("%1$-17s", info.medicalIssues?"YES":"NO")+" |");
+		System.out.println("|                                     |                                     |                                     |");
+		System.out.println("|=================================================================================================================|");
+	}
+
+	public static void displayQuotation(Quotation quotation) {
+		System.out.println(
+				"| Company: " + String.format("%1$-26s", quotation.company) + 
+				" | Reference: " + String.format("%1$-24s", quotation.reference) +
+				" | Price: " + String.format("%1$-28s", NumberFormat.getCurrencyInstance().format(quotation.price))+" |");
+		System.out.println("|=================================================================================================================|");
+	}
+
+    public static final ClientInfo[] clients = {
+		new ClientInfo("Niki Collier", ClientInfo.FEMALE, 49, 1.5494, 80, false, false),
+		new ClientInfo("Old Geeza", ClientInfo.MALE, 65, 1.6, 100, true, true),
+		new ClientInfo("Hannah Montana", ClientInfo.FEMALE, 21, 1.78, 65, false, false),
+		new ClientInfo("Rem Collier", ClientInfo.MALE, 49, 1.8, 120, false, true),
+		new ClientInfo("Jim Quinn", ClientInfo.MALE, 55, 1.9, 75, true, false),
+		new ClientInfo("Donald Duck", ClientInfo.MALE, 35, 0.45, 1.6, false, false)
+	};
 }
